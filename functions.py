c1fe435a30778dd8f56b25b6b142628faaf3ee20
douglasparism <douglasparism@gmail.com>
2021-11-04 22:20:58 -0600

    Q4, Q5, Q6 & Q7 Answered

--- functions.py
+++ functions.py
@@ -9,6 +9,7 @@
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
 from datetime import timedelta,datetime
+import pandas as pd
 
 def f_compare_ts(ts_list_o, ts_list_d):
     ts_list_o_dt = [datetime.strptime(i, "%Y-%m-%dT%H:%M:%S.%fZ") for i in ts_list_o]
@@ -24,4 +25,21 @@ def f_compare_ts(ts_list_o, ts_list_d):
     exact_matches = [i for i in unique_dates if (i in (ts_list_o_dt) and i in (ts_list_d_dt))]
     f_compare_ts['exact_match'] = {"qty":len(exact_matches), "values":exact_matches}
     return f_compare_ts
+
+def historical_spread(ob_data):
+    k = list(ob_data.keys())
+    lowest_asks = []
+    highest_bids = []
+    spreads = []
+    for i in k:
+        lowest_asks.append(ob_data[i].ask.min())
+        highest_bids.append(ob_data[i].bid.max())
+        spreads.append(abs(ob_data[i].ask.min() - ob_data[i].bid.max()))
+
+    df_ts_tob = pd.DataFrame()
+    df_ts_tob["timestamp"] = k
+    df_ts_tob["bid"] = highest_bids
+    df_ts_tob["ask"] = lowest_asks
+    df_ts_tob["spread"] = spreads
+    return df_ts_tob
 
--- visualizations.py
+++ visualizations.py
@@ -10,6 +10,8 @@
 """
 import plotly.express as px
 import pandas as pd
+import plotly.graph_objects as go
+from datetime import timedelta,datetime
 
 def orderbook_barchart(ob_data, levels):
     k = list(ob_data.keys())
@@ -30,4 +32,17 @@ def orderbook_barchart(ob_data, levels):
     long_df = pd.concat([bids.head(levels), asks.head(levels)])
     fig = px.bar(long_df, x="Value", y="Size", color="label", title="OrderBook TimeStamp: " + max_timestamp)
     fig.update_xaxes(type='category')
+    fig.show()
+
+def spread_graph(df_ts_tob):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df_ts_tob['timestamp'], y=df_ts_tob['bid'], name='Bid', mode='lines'))
+    fig.add_trace(go.Scatter(x=df_ts_tob['timestamp'], y=df_ts_tob['ask'], name='Ask', mode='lines'))
+    fig.update_layout(title='SPREAD GRAPH')
+    fig.show()
+
+def spread_boxplot(df_ts_tob):
+    df_ts_tob['timestamp'] = df_ts_tob['timestamp'].apply(lambda x: datetime.strptime(x, "%Y-%m-%dT%H:%M:%S.%fZ"))
+    df_ts_tob['hour'] = df_ts_tob['timestamp'].dt.hour
+    fig = px.box(df_ts_tob, x="hour", y="spread")
     fig.show()
